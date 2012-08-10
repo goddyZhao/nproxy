4a755c8fdde15423f893612bc54d2ca27444ad4f
Goddy Zhao <goddy128@gmail.com>
2012-08-10 13:55:08 +0800

    fix #1

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# CHANGELOG
+
+## v0.0.2
+
+    [+] support string pattern
+    [&] change the color of info level log to blue 
--- replace-rule.sample.js
+++ replace-rule.sample.js
@@ -2,13 +2,13 @@ module.exports = [
 
   // 1. replace single file
   {
-    pattern: /homepage\.js/,      // Match url you wanna replace
+    pattern: 'homepage.js',      // Match url you wanna replace
     responder:  "file path"
   },
 
   // 2. replace combo file with src with absolute file path
   {
-    pattern: /group\/homepageTileFramework.*\.js/,
+    pattern: 'group/homepageTileFramework.*.js', 
     responder: [
       '/home/goddyzhao/workspace/webapp/ui/homepage/js/a.js',
       '/home/goddyzhao/workspace/webapp/ui/homepage/js/b.js',
@@ -18,7 +18,7 @@ module.exports = [
 
   // 3. replace combo file with src with relative file path and specified dir
   {
-    pattern: /group\/homepageTileFramework.*\.js/,
+    pattern: 'group/homepageTileFramework.*.js',
     responder: {
       dir: '/home/goddyzhao/workspace/webapp/ui/homepage/js',
       src: [
