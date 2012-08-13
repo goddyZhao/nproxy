0be0e302f4de63d17f3e27fb68a9275e989f1343
Goddy Zhao <goddy128@gmail.com>
2012-08-13 20:40:46 +0800

    add test structures

---
+++ TODO.md
@@ -0,0 +1,5 @@
+#TODO
+
+## Features
+    
+    [+] support https proxy
---
+++ test/array.js
@@ -0,0 +1,5 @@
+describe('#indexOf()', function(){
+  it('should return -1 when not present', function(){
+    [1,2,3].indexOf(4).should.equal(-1);
+  });
+});
