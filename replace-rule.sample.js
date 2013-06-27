31b60545d139b7b9fedfa97915006327f9237d2d
Goddy Zhao <goddy128@gmail.com>
2013-06-27 11:44:54 +0800

    add docs for new feature

--- replace-rule.sample.js
+++ replace-rule.sample.js
@@ -39,5 +39,23 @@ module.exports = [
   {
     pattern: 'ui/homepage/img',  // must be a string
     responder: '/home/goddyzhao/image/' //must be a absolute directory path
+  },
+
+  // 6. Write responder with regular expression variables like $1, $2
+  {
+    pattern: /https?:\/\/[\w\.]*(?::\d+)?\/ui\/(.*)_dev\.(\w+)/,
+    reponder: 'http://localhost/proxy/$1.$2'
+  },
+
+  // 7. Map server image directory to local image directory with regular expression
+  // This simple rule can replace multiple directories to corresponding locale ones
+  // For Example, 
+  //   http://host:port/ui/a/img/... => /home/a/image/...
+  //   http://host:port/ui/b/img/... => /home/b/image/...
+  //   http://host:port/ui/c/img/... => /home/c/image/...
+  //   ...
+  {
+    pattern: /ui\/(.*)\/img\//,
+    responder: '/home/$1/image/'
   }
 ];
