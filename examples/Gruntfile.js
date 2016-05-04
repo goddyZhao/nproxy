69188c523400c29bee87bd59f8796bba30b1ae72
Stephane Tessier <stessier@clever-age.com>
2016-05-04 17:41:06 +0200

    Requests can now be proxyfied

--- examples/Gruntfile.js
+++ examples/Gruntfile.js
@@ -2,30 +2,38 @@
 // http://gruntjs.com/
 
 module.exports = function (grunt) {
-    'use strict';
-    var nproxy = require('nproxy'),
-        argv   = require('yargs').argv;
+  'use strict';
+  var nproxy = require('nproxy'),
+    argv   = require('yargs').argv;
 
 
-    grunt.registerTask('nproxy', 'start nproxy to proxy local JS and CSS files', function() {
+  grunt.registerTask('nproxy', 'start nproxy to proxy local JS and CSS files', function() {
 
-        // Grunt should not finish the task
-        this.async();
+    // Grunt should not finish the task
+    this.async();
 
-        // This will serve static local files for JS & CSS intead of remote ones
-        // use: `localhost:8989` as proxy parameter
+    // This will serve static local files for JS & CSS intead of remote ones
+    // use: `localhost:8989` as proxy parameter
 
-        // see `nproxy-conf.js` for proxy configuration
-        var port = 8989,
-            options = {
-                timeout: 100,
-                debug: false
-            };
+    // see `nproxy-conf.js` for proxy configuration
+    var port = 8989,
+      options = {
+        timeout : 100,
+        debug   : !!argv.debug
+      };
 
-        options.responderListFilePath = require('./nproxy-conf.js')(argv);
+    // OPTIONAL: If you want nproxy requests to be proxyfied
+    options.proxy = {
+      host   : 'proxyhost',                                // mandatory
+      port   : 80,                                         // optional, default to 80
+      auth   : 'login:pass',                               // optional
+      bypass : ['localhost', 'dev.localhost', 'bypass.me'] // optional, default to ['127.0.0.1', 'localhost']
+    };
 
-        nproxy(port, options);
-    });
+    options.responderListFilePath = require('./nproxy-conf.js')(argv);
+
+    nproxy(port, options);
+  });
 
 };
 
--- examples/gulpfile.js
+++ examples/gulpfile.js
@@ -16,9 +16,17 @@ gulp.task('nproxy', function nproxyTask(host) {
 
   var port = 8989,
       options = {
-        timeout: 100,
-        debug: false
+				timeout : 100,
+				debug   : !!argv.debug
       };
+
+  // OPTIONAL: If you want nproxy requests to be proxyfied
+  options.proxy = {
+    host   : 'proxyhost',                                // mandatory
+    port   : 80,                                         // optional, default to 80
+    auth   : 'login:pass',                               // optional
+    bypass : ['localhost', 'dev.localhost', 'bypass.me'] // optional, default to ['127.0.0.1', 'localhost']
+  };
 
   options.responderListFilePath = require('./nproxy-conf.js')(argv);
 
